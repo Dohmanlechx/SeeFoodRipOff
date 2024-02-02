e2124900d14f4106aee17402c8c59dc1c90b03f4
David Öhman <david.ohman@umain.com>
2024-02-02 17:47:35 +0100

    Setup

---
+++ lib/model_3d_viewer.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Model3dViewer extends StatelessWidget {
+  const Model3dViewer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
