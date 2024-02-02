6daca5c1b23e95f4793188975049840a69e8a3c9
Cas van Luijtelaar <cas.luijtelaar@umain.com>
2024-02-02 15:12:58 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2023 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:object_detection_ssd_mobilenet/object_detection.dart';
+import 'dart:io' show Platform;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home:  SeeFood(),
+    );
+  }
+}
+
+class SeeFood extends StatefulWidget {
+  const SeeFood({super.key});
+
+  @override
+  State<SeeFood> createState() => _SeeFoodState();
+}
+
+class _SeeFoodState extends State<SeeFood> {
+  final imagePicker = ImagePicker();
+
+  ObjectDetection? objectDetection;
+  Uint8List? image;
+  bool? hotDog;
+
+  String buildHotDogString() {
+    if(hotDog == null) return "";
+    return hotDog == true ? "Hot Dog" : "Not Hot Dog";
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    objectDetection = ObjectDetection();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: (image != null) ? Image.memory(image!) : const SizedBox.shrink(),
+              ),
+            ),
+            Text(buildHotDogString(), style: const TextStyle(fontSize: 40)),
+            
+            SizedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  if (Platform.isAndroid || Platform.isIOS)
+                    IconButton(
+                      onPressed: () async {
+                        final result = await imagePicker.pickImage(
+                          source: ImageSource.camera,
+                        );
+
+                        if (result == null) return ;
+                        final data = objectDetection!.analyseImage(result.path);
+
+                        setState(() {
+                          image = data.image;
+                          hotDog = data.isHotDog;
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.camera,
+                        size: 64,
+                      ),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/result.dart
@@ -0,0 +1,8 @@
+import 'dart:typed_data';
+
+class Result {
+  final Uint8List image;
+  final bool isHotDog;
+
+  Result(this.image, this.isHotDog);
+}
